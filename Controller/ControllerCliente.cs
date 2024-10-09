0124359ad3592385a4455dc0527836a35c0cc7f3
Arthur K <arthurk031@outlook.com>
2024-10-08 21:56:32 -0300

    Implementação de Controller

---
+++ Controller/ControllerCliente.cs
@@ -0,0 +1,23 @@
+﻿using CRUD.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CRUD.Controller
+{
+    internal class ControllerCliente
+    {
+        public void Add(string nome, string email, DateTime dataNascimento)
+        {
+            if (!String.IsNullOrWhiteSpace(nome) && !String.IsNullOrWhiteSpace(email) && dataNascimento != null)
+            {
+                Cliente cliente = new Cliente()
+                {
+                    Nome = nome,
+                    Email = email,
+                    DataNascimento = dataNascimento
+                };
+            }
+        }
+    }
+}
--- Domain/Entities/Cliente.cs
+++ Domain/Entities/Cliente.cs
@@ -4,16 +4,11 @@ using System.Text;
 
 namespace CRUD.Domain.Entities
 {
-    class Cliente
+    public class Cliente
     {
-        public int Id { get; set; }
         public string Nome { get; set; }
         public DateTime DataNascimento { get; set; }
-        public bool Ativo { get; set; }
         public string Email { get; set; }
-        public Cliente()
-        {
-            Ativo = true;
-        }
+        public Cliente() { }
     }
 }
